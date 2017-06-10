d9ae4260825505ea877c40eddab142e7512be159
cartier1288 <cartier1288@gmail.com>
2017-06-10 11:23:35 -0400

    First commit.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html>
+  <head>
+    <title>Introduction to Canadian Law</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="./css/style.css" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=1100; initial-scale=1.0; target-densityDpi=device-dpi" />
+
+    <meta name="keywords" content="Canadian, Law, Introduction, Overview, Summary, Interconnected" />
+    <meta name="description" content="An introduction to Canadian Law." />
+    <meta name="author" content="Isaac McQuaide" />
+    <meta name="robots" content="all" />
+
+    <link rel="shortcut icon" href="img/favicon.ico" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+    <script src="../javascript/*.js"></script>
+  </head>
+
+  <body>
+    <h1>Temporary Links</h1>
+    <a href="introtolaw.php">Introduction to Law</a>
+  </body>
+</html>
